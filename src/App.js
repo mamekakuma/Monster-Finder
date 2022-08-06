d59784cd8ea5339b5638877c9ce8c417d4e9fde1
mamekakuma <mameabera5@gmail.com>
2022-08-06 08:34:50 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** @format */
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id : 'id1'
+        },
+        {
+          name: 'Dracula',
+          id : 'id2'
+        },
+        {
+          name: 'Zombie',
+          id : 'id3'
+        },
+        {
+          name: 'MaMe',
+          id : 'id4'
+        },
+      ],
+    };
+  }
+  componentDidMount(){
+  }
+  render() {
+    return <div className="App">
+      {
+        this.state.monsters.map(monster => <h1 key = {monster.id}>{monster.name}</h1>)
+      }
+    </div>;
+  }
 }
-
+com
 export default App;
