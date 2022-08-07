28cf37bb14906b3ed402f5b7f34cbdbce9d49f48
mamekakuma <mameabera5@gmail.com>
2022-08-08 02:37:57 +0300

    The monster layouts

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 /** @format */
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
+import { CardList } from './components/card-list/card-list.component';
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id : 'id1'
-        },
-        {
-          name: 'Dracula',
-          id : 'id2'
-        },
-        {
-          name: 'Zombie',
-          id : 'id3'
-        },
-        {
-          name: 'MaMe',
-          id : 'id4'
-        },
-      ],
+      monsters: [],
     };
   }
-  componentDidMount(){
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return <div className="App">
-      {
-        this.state.monsters.map(monster => <h1 key = {monster.id}>{monster.name}</h1>)
-      }
+      <CardList monsters = {this.state.monsters}/> 
+     
     </div>;
   }
 }
-com
 export default App;
