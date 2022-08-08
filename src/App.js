f598701ed5d79da166f7ab1668b4260175980d4a
mamekakuma <mameabera5@gmail.com>
2022-08-08 04:42:03 +0300

    Search Bar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import "./App.css";
 import { Component } from "react";
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component' ;
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: ""
     };
+    // this.handleChange = this.handleChange.bind(this) ;
+  }
+   handleChange =  e => {
+    this.setState({ searchField: e.target.value });
   }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -15,9 +21,17 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return <div className="App">
-      <CardList monsters = {this.state.monsters}/> 
-     
+      <SearchBox
+        placeholder="Search monster"
+        handleChange={this.handleChange }
+      />
+      <CardList monsters={filteredMonsters} />
+
     </div>;
   }
 }
--- src/components/card-list/card/card.component.jsx
+++ src/components/card-list/card/card.component.jsx
@@ -4,5 +4,6 @@ export const Card = (props) => (
     <div className="card-container">
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster hidden somewhere" />
         <h1 > {props.monster.name} </h1>  
+        <p>{props.monster.email}</p>
     </div>
 )
